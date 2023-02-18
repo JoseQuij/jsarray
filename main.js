15074b90425495b9c5a87ad91b1c9d1f622ad84c
Jose Quijije <josequijije5@gmail.com>
2023-02-17 22:25:26 -0600

    arrays

---
+++ main.js
@@ -0,0 +1,25 @@
+const items = [
+    {name: 'bike', price: 100},
+    {name: 'car', price: 100},
+    {name: 'unicycle', price: 120},
+    {name: 'book', price: 110},
+    {name: 'apple', price: 30},
+    {name: 'keyboard', price: 200},
+]
+
+// const filteredItems = items.filter((items) => {
+//     return items.price <= 100
+// })
+// const mappedItem = items.map((e) => {
+//     return e.name
+// })
+// const foundItem = items.find((e) => {
+//     return e.price === 120
+// })
+// items.forEach((item) => {
+//     console.log(item.name)
+// })
+// const total = items.reduce((lol, item) => {
+//     return item.price + lol
+// }, 0 )
+console.log(total)
